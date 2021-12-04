3bbbf93112aba2fc3bc398577308694d33c419e5
RadSalty <mbsulcs@gmail.com>
2021-12-04 22:22:24 +0800

    added comments

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -8,10 +8,12 @@ from scripts.helpful_scripts import (
 
 def deploy_fund_me():
     account = get_account()
+    # Checks if we are deploying to a local or testnet, if testnet use defined chainlink oracles defined in brownie-config
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
+    # If a local deployment deploy the mock oracles
     else:
         deploy_mocks()
         price_feed_address = MockV3Aggregator[-1].address
@@ -20,7 +22,7 @@ def deploy_fund_me():
         price_feed_address,
         {"from": account},
         publish_source=config["networks"][network.show_active()].get("verify"),
-    )
+    )  # uses etherscan api to verify the contract
     print(f"Contract deployed to {fund_me.address}")
     return fund_me
 
